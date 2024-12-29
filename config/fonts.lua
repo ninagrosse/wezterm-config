7597ff9ab31c5548bf425298de1d330fcef01d11
Nina Große <nina.grosse@posteo.de>
2024-12-29 15:03:05 +0100

    fonts: add fallback font (WezTerm's default font)

--- config/fonts.lua
+++ config/fonts.lua
@@ -6,10 +6,11 @@ local font_family = 'JetBrainsMono Nerd Font'
 local font_size = platform.is_mac and 14 or 11
 
 return {
-   font = wezterm.font({
-      family = font_family,
-      weight = 'Medium',
-   }),
+   font = wezterm.font_with_fallback {
+      { family = font_family, weight = 'Medium' },
+      'JetBrains Mono',
+   },
+
    font_size = font_size,
 
    --ref: https://wezfurlong.org/wezterm/config/lua/config/freetype_pcf_long_family_names.html#why-doesnt-wezterm-use-the-distro-freetype-or-match-its-configuration
